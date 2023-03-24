8a5bfe026840f1d2d79596aa5068076d40e26924
Willian Silvestre <silvestrew392@gmail.com>
2023-03-24 16:45:37 -0300

    index atualizado

--- listarBanco/index.php
+++ listarBanco/index.php
@@ -10,25 +10,26 @@
 
 <body>
 
-    <?php 
-    function listarRegistros() {
+    <?php
+    function listarRegistros()
+    {
         // Estabelecendo a conexão com o banco de dados
         $localhost = "localhost";
         $user = "root";
         $password = "";
         $dbname = "listardados";
-    
+
         $conn = mysqli_connect($localhost, $user, $password, $dbname);
-    
+
         // Verificando se a conexão foi estabelecida com sucesso
         if (!$conn) {
             die("Conexão falhou: " . mysqli_connect_error());
         }
-    
+
         // Executando a query SELECT para buscar os registros do banco de dados
         $query = "SELECT * FROM user";
         $result = mysqli_query($conn, $query);
-    
+
         // Verificando se a query retornou resultados
         if (mysqli_num_rows($result) > 0) {
             // Exibindo os registros retornados pela query
@@ -41,7 +42,33 @@
         } else {
             echo "<tr><td colspan='2'>Nenhum registro encontrado</td></tr>";
         }
-    
+
+        // Fechando a conexão com o banco de dados
+        mysqli_close($conn);
+    }
+
+    function adicinarRegistros()
+    {
+        // Estabelecendo a conexão com o banco de dados
+        $localhost = "localhost";
+        $user = "root";
+        $password = "";
+        $dbname = "listardados";
+        $conn = mysqli_connect($localhost, $user, $password, $dbname);
+
+        // Inserindo informações no banco de dados
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+
+            $sql = "INSERT INTO user (name, email) VALUES ('$name', '$email')";
+
+            if (mysqli_query($conn, $sql)) {
+                echo "Informações inseridas com sucesso";
+            } else {
+                echo "Erro: " . $sql . "<br>" . mysqli_error($conn);
+            }
+        }
         // Fechando a conexão com o banco de dados
         mysqli_close($conn);
     }
@@ -49,30 +76,41 @@
 
     <div class="container">
         <h1 class="my-4">Exemplo de Lista:</h1>
-        <form action="inserir.php" method="post">
-            <div class="form-group">
+        <form method="post" class="row">
+            <div class="form-group col">
                 <label for="name">Nome:</label>
-                <input type="text" class="form-control" id="name" name="name">
+                <input type="text" class="form-control form-control-sm" id="name" name="name">
             </div>
-            <div class="form-group">
-                <label for="email">email:</label>
-                <input type="text" class="form-control" id="email" name="email">
+            <div class="form-group col">
+                <label for="email">Email:</label>
+                <input type="text" class="form-control form-control-sm" id="email" name="email">
             </div>
-            <button type="submit" class="btn btn-primary">Enviar</button>
+            <div class="form-group col-auto">
+                <button type="submit" class="btn btn-primary" name="submit">Enviar</button>
+                <?php
+                if (isset($_POST['submit'])) {
+                    adicinarRegistros();
+                }
+                ?>
+            </div>
         </form>
         <hr>
         <h2 class="my-4">Lista de dados:</h2>
-        <table class="table table-bordered table-striped">
-            <thead>
-                <tr>
-                    <th>Nome</th>
-                    <th>Email</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php listarRegistros(); ?>
-            </tbody>
-        </table>
+        <div class="row">
+            <div class="col">
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php listarRegistros(); ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </body>
 
